0fbe358be2a486b359aa1c8c425dcd4ca335fc70
Thiago <tiagohfs.cruz@gmail.com>
2024-08-03 16:45:39 -0300

    feat: Apredendo os conceitos basicos de JS

---
+++ exemplo.js
@@ -0,0 +1,24 @@
+// escreve comentários
+
+var a
+let b
+const c = 5
+
+a = 123
+b = 'Macaco'
+
+console.log(a)
+console.log(b)
+console.log(c)
+
+function SomarNumeros(num1 , num2) {
+    return num1 + num2
+}
+
+console.log(SomarNumeros(20,100))
+
+function QuantidadeCaracter(palavra){
+    return palavra.lenght
+}
+
+console.log(QuantidadeCaracter('Sua Mãe'))
